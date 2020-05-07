bb56ccd840ea0fbd111cc27af0266cf422de7f1f
Isaac Scarisbrick <zfac143@live.rhul.ac.uk>
2020-05-07 14:26:36 +0100

    init

---
+++ src/KmpFind.java
@@ -0,0 +1,36 @@
+public class KmpFind {
+  
+  /**
+   * Find a word in a string.
+   * @param s the string to search
+   * @param w the word to find in the string.
+   * @return Returns true or false.
+   */
+  public boolean find(String s, String w) {
+    int k = 0;
+    char[] sc = s.toCharArray();
+    char[] ws = w.toCharArray();
+    for (char c : sc) {
+      if (ws[k] == c) {
+        k++;
+        if (k == ws.length) {
+          System.out.println("Word is in string.");
+          return true;
+        }
+      } else {
+        k = 0;
+      }
+    }
+    System.out.println("Word is not in string.");
+    return false;
+  }
+  
+  /**
+   * Testing driver code.
+   * @param args User arguments.
+   */
+  public static void main(String[] args) {
+    KmpFind kmpfind= new KmpFind();
+    kmpfind.find("TestMe", "Test");
+  }
+}
